5c5b7252a5e1e4d0a6c6391feaef3dc8eff60d50
Progammer <programmer@nbadigitalworlds.com>
2023-10-03 13:10:09 +0700

    Update category.js

--- routes/product/category.js
+++ routes/product/category.js
@@ -6,7 +6,7 @@ const authAdmin = require("../../lib/auth.admin");
 //create category
 router.post("/create", authAdmin, category.create);
 router.get("/list", authAdmin, category.GetAll);
-router.get("member/list", auth, category.GetAll);
+router.get("/member/list", auth, category.GetAll);
 router.get("/:id", authAdmin, category.GetById);
 router.get("/member/:id", auth, category.GetById);
 router.put("/update/:id", authAdmin, category.UpdateById);
