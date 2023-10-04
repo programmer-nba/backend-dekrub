95221b6e5044edd4475a39e8e78a674c1d0ac8d3
Progammer <programmer@nbadigitalworlds.com>
2023-10-04 17:17:13 +0700

    Update index.js

--- routes/commission/index.js
+++ routes/commission/index.js
@@ -5,12 +5,14 @@ const authAdmin = require('../../lib/auth.admin');
 
 //Commission Day
 router.get("/day", auth, commission.GetAllDay);
+router.get("/admin/day", authAdmin, commission.GetAllDay);
 router.post("/day", auth, commission.WithdrawDay);
 router.put("/day/:id", authAdmin, commission.confirmDay);
 router.put("/day/cancel/:id", authAdmin, commission.cancelDay);
 
 //Commission Week
 router.get("/week", auth, commission.GetAllWeek);
+router.get("/admin/week", authAdmin, commission.GetAllWeek);
 router.post("/week", auth, commission.WithdrawWeek);
 router.put("/week/:id", authAdmin, commission.confirmWeek);
 router.put("/week/cancel/:id", authAdmin, commission.cancelWeek);
