56e9a52cfb2e0169a75d0bf3e8aad3b504a62688
Progammer <programmer@nbadigitalworlds.com>
2023-10-05 17:08:39 +0700

    Update order.status.controller.js

--- controllers/order/order.status.controller.js
+++ controllers/order/order.status.controller.js
@@ -5,11 +5,15 @@ const {
 const {
   Commission_week,
 } = require("../../models/commission/commission.week.model.js");
+const {
+  Percent_Commission,
+} = require("../../models/commission/percent.commission.model.js");
 const {Members} = require("../../models/member.model/member.model.js");
 
 //confirm order
 module.exports.confirm = async (req, res) => {
   const updateStatus = await OrderProductModel.findOne({_id: req.params.id});
+  const percent = await Percent_Commission.findOne({code: "administer"});
   if (updateStatus) {
     updateStatus.status.push({
       status: "ยืนยันออเดอร์",
@@ -19,17 +23,52 @@ module.exports.confirm = async (req, res) => {
     const member = await Members.findOne({
       member_number: updateStatus.member_number,
     });
-    const upline = [member.upline.lv1];
-    
-    const commission = 10;
-    const vat3percent = (commission * 3) / 100;
-    const remainding_commission = commission - vat3percent;
+
+    const upline = [member.upline];
+
+    const commission_level1 = percent.level_two;
+    const vat_level1 = (commission_level1 * 3) / 100;
+    const remainding_commission_level1 = commission_level1 - vat_level1;
+
+    const commission_level3 = percent.level_three;
+    const vat_level3 = (commission_level3 * 3) / 100;
+    const remainding_commission_level3 = commission_level3 - vat_level3;
+
+    for (const TeamMemberData of upline) {
+      if (TeamMemberData.lv1) {
+        const storeData = [];
+        const integratedData = {
+          member_number: TeamMemberData.lv1,
+          commission: commission_level1,
+          vat3percent: vat_level1,
+          remainding_commission: remainding_commission_level1,
+        };
+        if (integratedData) {
+          storeData.push(integratedData);
+        }
+        const commissionData = {
+          data: storeData,
+          from_member: updateStatus.member_number,
+        };
+        const commission_week = new Commission_week(commissionData);
+        commission_week.save();
+        const member2 = await Members.findOne({
+          member_number: TeamMemberData.lv1,
+        });
+        const new_commission_week =
+          member2.commission_week + remainding_commission_level1;
+        await Members.findByIdAndUpdate(member2._id, {
+          commission_week: new_commission_week,
+        });
+      }
+    }
+
     const storeData = [];
     const integratedData = {
-      member_number: upline[0],
-      commission: commission,
-      vat3percent: vat3percent,
-      remainding_commission: remainding_commission,
+      member_number: updateStatus.member_number,
+      commission: commission_level3,
+      vat3percent: vat_level3,
+      remainding_commission: remainding_commission_level3,
     };
     if (integratedData) {
       storeData.push(integratedData);
@@ -41,9 +80,10 @@ module.exports.confirm = async (req, res) => {
     const commission_week = new Commission_week(commissionData);
     commission_week.save();
     const member2 = await Members.findOne({
-      member_number: upline[0],
-    })
-    const new_commission_week = member2.commission_week + remainding_commission;
+      member_number: updateStatus.member_number,
+    });
+    const new_commission_week =
+      member2.commission_week + remainding_commission_level1;
     await Members.findByIdAndUpdate(member2._id, {
       commission_week: new_commission_week,
     });
