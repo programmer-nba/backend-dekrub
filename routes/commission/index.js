b3806b97cb45950b40fd59f8614298a125ea5b28
Progammer <programmer@nbadigitalworlds.com>
2023-10-10 14:19:28 +0700

    update

--- routes/commission/index.js
+++ routes/commission/index.js
@@ -3,20 +3,15 @@ const commission = require('../../controllers/commission/commission.controller')
 const auth = require('../../lib/auth');
 const authAdmin = require('../../lib/auth.admin');
 
-//Commission Day
-router.get("/day", auth, commission.GetAllDay);
-router.get("/admin/withdrawday", authAdmin, commission.GetWithdrawDay);
-router.get("/admin/day", authAdmin, commission.GetAllDay);
-router.post("/day", auth, commission.WithdrawDay);
-router.put("/day/:id", authAdmin, commission.confirmDay);
-router.put("/day/cancel/:id", authAdmin, commission.cancelDay);
+//Commission Day Register
+router.get("/day", commission.GetAllDay);
 
 //Commission Week
-router.get("/week", auth, commission.GetAllWeek);
-router.get("/admin/withdrawweek", authAdmin, commission.GetWithdrawWeek);
-router.get("/admin/week", authAdmin, commission.GetAllWeek);
-router.post("/week", auth, commission.WithdrawWeek);
-router.put("/week/:id", authAdmin, commission.confirmWeek);
-router.put("/week/cancel/:id", authAdmin, commission.cancelWeek);
+router.get("/week", commission.GetAllWeek);
+
+//Commission Week Register
+router.get("/register/week", commission.GetAllWeekRegister);
+
+
 
 module.exports = router;
