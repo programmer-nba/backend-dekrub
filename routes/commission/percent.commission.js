0e7f7b3679e2c54f01075739c13b11674d70226a
Progammer <programmer@nbadigitalworlds.com>
2023-10-11 11:51:02 +0700

    Update percent.commission.js

--- routes/commission/percent.commission.js
+++ routes/commission/percent.commission.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const PercentCommission = require('../../controllers/commission/percent.commission.controller');
 const authAdmin = require('../../lib/auth.admin');
 
-//commission 
+//commission
+router.post('/commission', authAdmin, PercentCommission.create);
 router.get("/commission", authAdmin, PercentCommission.GetAll);
 router.get("/commission/:id", authAdmin, PercentCommission.GetAllByid);
 router.put("/commission/:id", authAdmin, PercentCommission.Update);
