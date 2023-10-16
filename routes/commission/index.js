3d5738f61a595a2bbb749c01f522a143eec0fee5
Progammer <programmer@nbadigitalworlds.com>
2023-10-16 12:08:01 +0700

    Update index.js

--- routes/commission/index.js
+++ routes/commission/index.js
@@ -10,12 +10,12 @@ router.delete("/day/:id",authAdmin, commission.deleteDay);
 
 //Commission Week
 router.get("/week", commission.GetAllWeek);
-router.put("/day/:id", authAdmin, commission.updateWeek);
+router.put("/week/:id", authAdmin, commission.updateWeek);
 router.delete("/week/:id",authAdmin, commission.deleteWeek);
 
 //Commission Week Register
 router.get("/register/week", commission.GetAllWeekRegister);
-router.put("/day/:id", authAdmin, commission.updateWeekRegister);
+router.put("/register/week/:id", authAdmin, commission.updateWeekRegister);
 router.delete("/register/week/:id",authAdmin, commission.deleteWeekRegister);
 
 
